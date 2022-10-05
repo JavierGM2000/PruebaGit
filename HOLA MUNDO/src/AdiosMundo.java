fef6bd0bf0aeb54947a0cb8c8a5bb2ef34fb6512
Javier Gómez <gomez.javier@opendeusto.es>
2022-10-05 09:03:39 +0200

    Cambiado mensaje salida

--- HOLA MUNDO/src/AdiosMundo.java
+++ HOLA MUNDO/src/AdiosMundo.java
@@ -3,7 +3,7 @@ public class AdiosMundo {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("ADIOS MUNDO");
+		System.out.println("HOLA MUNDOMUNDO");
 	}
 
 }
